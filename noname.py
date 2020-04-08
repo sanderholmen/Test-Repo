a2da9c1119a2ad4ff8bd80f0d843108f9031a22c
emilbratt <emilbratt@gmail.com>
2020-04-08 09:07:11 +0200

    Learning about the random module as well as a little more playing with the in range function

--- noname.py
+++ noname.py
@@ -176,15 +176,70 @@
 # print(float(randomVariable5(5)))
 # print(int(randomVariable5(21)))
 
+
 #Taking another look at error handeling.
 #This program fixes this by using the try and except statements.
-print('Type a number, please!')
-myNumber = input()
+# print('Type a number, please!')
+# myNumber = input()
+#
+# try:
+#     if int(myNumber) > 5:
+#         print('Thats a high number!')
+#     else:
+#         print('Thats a low number!')
+# except ValueError:
+#     print('You did not type in a number!')
 
-try:
-    if int(myNumber) > 5:
-        print('Thats a high number!')
-    else:
-        print('Thats a low number!')
-except ValueError:
-    print('You did not type in a number!')
+
+# #Using the random module to make a guessing game.
+# import random #We import the random module..
+# print('This is a guessing game!')
+# secretVariable1 = random.randint(1, 20) #We set the variable "secretVariable1" to a random number between 1 and 20..
+# print('Guess a number between 1 and 20!')
+#
+# for guessRemainingVariable in range(1, 7): #We set a range of 0 to 6 (not counting 7 as you remember) giving us 1 to 6 -> giving us 6 tries.
+#                                            #This will run one line at a time in the for loop up to 6 times unless you guess correct.
+#     print('Now, guess the number!')
+#     guessVariable1 = int(input()) #We store the number that we guess as an integer in the "guessVariable1"..
+#     if guessVariable1 < secretVariable1: #If number we guess is lower than the number that is random..
+#         print('You have ' + str(int(6 - (guessRemainingVariable))) + ' tries left..!')
+#         print('To low!')
+#     elif guessVariable1 > secretVariable1: #If number that we guess is higher than the number that is random..
+#         print('You have ' + str(int(6 - (guessRemainingVariable))) + ' tries left..!')
+#         print('To high!')
+#     else:
+#         break #This condition would be met if your guess is correct. This takes us out of the "for" loop and move us on to the next block.
+#
+# if guessVariable1 == secretVariable1:
+#     print('Your guess is correct, my friend. Congratulations!')
+# else:
+#     print('You ran out of tries, Im sorry. Good-bye!')
+
+
+
+
+#Learning about the random function in python. A simple example usage would be a guessing game..
+#I will also implement the "in range" function to limit the amount of tries the user have to guess the correct answer.
+#Using the random module to make a guessing game would look something like this..
+# import random #We import the random module..
+# print('This is a guessing game!')
+# secretVariable1 = random.randint(1, 20) #We set the variable "secretVariable1" to a random number between 1 and 20..
+# print('Guess a number between 1 and 20!')
+#
+# for guessRemainingVariable in range(1, 7): #We set a range of 0 to 6 (not counting 7 as you remember) giving us 1 to 6 -> giving us 6 tries.
+#                                            #This will run one line at a time in the for loop up to 6 times unless you guess correct.
+#     print('Now, guess the number!')
+#     guessVariable1 = int(input()) #We store the number that we guess as an integer in the "guessVariable1"..
+#     if guessVariable1 < secretVariable1 and guessRemainingVariable <= 5: #If number we guess is lower than the number that is random and we have counted less than 0-5 (6 tries)..
+#         print('You have ' + str(int(6 - (guessRemainingVariable))) + ' tries left..!')
+#         print('To low, my friend, try again..!')
+#     elif guessVariable1 > secretVariable1 and guessRemainingVariable <= 5: #If number that we guess is higher than the number that is random and we have counted less than 0-5 (6 tries)..
+#         print('You have ' + str(int(6 - (guessRemainingVariable))) + ' tries left..!')
+#         print('To high, my friend, try again..!')
+#     else:
+#         break #This condition would be met if your guess is correct. This takes us out of the "for" loop and move us on to the next block.
+#
+# if guessVariable1 == secretVariable1:
+#     print('Your guess is correct, my friend. Congratulations!')
+# else:
+#     print('You ran out of tries. The number was ' + str(secretVariable1) + '.. Good-bye!')
